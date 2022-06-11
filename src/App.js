ea8ab96fde9872a9a80adc59ff377b52d2293207
joyfulpebble <danmax32100@gmail.com>
2022-06-11 23:05:08 +0300

    Управляемый input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 function App() {
   // let count = 0;
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState('Text.')
 
   function increment() {
     setCount( count + 1);
@@ -15,6 +16,14 @@ function App() {
   return (
     <div className="App">
       <h1>{count}</h1>
+      <h2>{value}</h2>
+
+      <input
+        type="text"
+        value={value}
+        onChange={element => setValue(element.target.value)}
+      />
+
       <button onClick = { increment }>Increment</button>
       <button onClick = { decrement }>Decrement</button>
     </div>
