f609713499d282a80dab722103fab1b5e068d07e
joyfulpebble <danmax32100@gmail.com>
2022-06-11 23:36:25 +0300

    Компоненты: классовый

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   const [value, setValue] = useState('Text.')
 
   return (
     <div className="App">
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
