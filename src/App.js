18c9e4dfbdb34795ca7ed9d556010c7486e9f3ff
joyfulpebble <danmax32100@gmail.com>
2022-06-11 23:17:19 +0300

    Компоненты: функциональный

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  // let count = 0;
-  const [count, setCount] = useState(0);
   const [value, setValue] = useState('Text.')
-
-  function increment() {
-    setCount( count + 1);
-  }
-
-  function decrement() {
-    setCount( count - 1)
-  }
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h2>{value}</h2>
-
-      <input
-        type="text"
-        value={value}
-        onChange={element => setValue(element.target.value)}
-      />
-
-      <button onClick = { increment }>Increment</button>
-      <button onClick = { decrement }>Decrement</button>
+      <Counter/>
     </div>
   );
 }
