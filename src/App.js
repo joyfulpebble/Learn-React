816d6e487fa58cc4e561cf197b705627ff75402d
joyfulpebble <danmax32100@gmail.com>
2022-06-11 22:57:53 +0300

    Работа с состоянием useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  // let count = 0;
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount( count + 1);
+  }
+
+  function decrement() {
+    setCount( count - 1)
+  }
+
   return (
     <div className="App">
-
+      <h1>{count}</h1>
+      <button onClick = { increment }>Increment</button>
+      <button onClick = { decrement }>Decrement</button>
     </div>
   );
 }
