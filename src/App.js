c246061641ac45cb38db658c01c46ef0c9ffbb7a
joyfulpebble <danmax32100@gmail.com>
2022-06-13 00:55:52 +0300

    Новое нРеÐализована фильтрация по title/body

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, {useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import Select from "./components/UI/select/Select";
 
 function App() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  // const bodyInputRef = useRef()
-
   const [posts, setPosts] = useState([
     {id: 1, title: 'Пример заголовка', body: 'Пример информации о посте'},
   ]);
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -18,10 +18,26 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+  const sortPosts  = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <Select
+            value={selectedSort}
+            onChange={sortPosts}
+            defaultValue={'Сортировка'}
+            options={[
+              {value: 'title', name: 'По названию'},
+              {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
       {
         posts.length !== 0
         ?
@@ -29,7 +45,6 @@ function App() {
         :
           <h1 style={{textAlign: 'center'}}>Упс! Постов нет :(</h1>
       }
-
     </div>
   );
 }
