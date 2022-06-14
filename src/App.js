151de7f144fe9445590ed29c1dbf244ba56760d9
joyfulpebble <danmax32100@gmail.com>
2022-06-14 21:25:29 +0300

    Добавлен поиск постов по тайтлу

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import Select from "./components/UI/select/Select";
+import CustomInput from "./components/UI/input/CustomInput";
 
 function App() {
   const [title, setTitle] = useState('');
@@ -10,24 +11,40 @@ function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: 'Пример заголовка', body: 'Пример информации о посте'},
   ]);
-  const [selectedSort, setSelectedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('')
 
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
+    setPosts([...posts, newPost]);
+  };
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
+    setPosts(posts.filter(p => p.id !== post.id));
+  };
   const sortPosts  = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
-  }
+  };
+
+  const sortedPosts = useMemo(() => {
+    console.log('ok')
+    if (selectedSort){
+      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]));
+    }
+    return posts;
+  }, [selectedSort, posts]);
+  const searchedAndSortedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
       <div>
+        <CustomInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder='Поиск...'
+        />
         <Select
             value={selectedSort}
             onChange={sortPosts}
@@ -39,9 +56,9 @@ function App() {
         />
       </div>
       {
-        posts.length !== 0
+        searchedAndSortedPosts.length !== 0
         ?
-          <PostList remove={removePost} posts={posts} title={'Список постов 1'}/>
+          <PostList remove={removePost} posts={searchedAndSortedPosts} title={'Список постов 1'}/>
         :
           <h1 style={{textAlign: 'center'}}>Упс! Постов нет :(</h1>
       }
