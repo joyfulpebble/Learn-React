8393f83750be0696c8879d0c9a59471c48673644
joyfulpebble <danmax32100@gmail.com>
2022-06-20 21:00:42 +0300

    Добавлен навбар и подгрузка контента без перезагрузки страницы

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import './styles/App.css'
 import * as React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Posts from "./pages/Posts.jsx";
+
 function App() {
   return (
     <BrowserRouter>
+      <div className="navbar">
+        <Link className="navbar__item" to="/posts">Posts</Link>
+        <Link className="navbar__item" to="/">Main</Link>
+      </div>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/posts' element={<Posts/>}/>
