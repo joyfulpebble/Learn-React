d908f944d7465b5c576abf0ed29aed92b941eeab
joyfulpebble <danmax32100@gmail.com>
2022-06-26 20:54:01 +0300

    Динамическая пагинация

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -12,19 +12,22 @@ import {useFetching} from "../hooks/useFetching";
 import {getPageCount} from "../utils/pages";
 import {usePagination} from "../hooks/usePagination";
 import Pagination from "../components/Pagination.jsx"
+import { useRef } from "react";
 
 function Posts() {
-  const [title, setTitle]   = useState('');
-  const [body, setBody]     = useState('');
-  const [posts, setPosts]   = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal]   = useState(false);
+  const [modal, setModal] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit]   = useState(10);
-  const [page, setPage]     = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const lastElement = useRef(null);
+  let observer = useRef();
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPageCount(totalCount, limit))
   });
@@ -41,33 +44,59 @@ function Posts() {
 
   const searchedAndSortedPosts = usePost(posts, filter.sort, filter.query);
 
+  useEffect(() => {   
+    if( isPostsLoading ) return;
+    const last = lastElement.current;
+    
+    let callback = function(entries, observer) {
+      if(entries[0].isIntersecting && page < totalPages){
+        setPage(page + 1);
+      }
+    }
+    
+    observer = new IntersectionObserver(callback);
+    if( last ) observer.observe(last);
+    return () => {
+      if( last ) observer.unobserve(last);
+    }
+  }, [isPostsLoading])
+
   useEffect(() => {
     fetchPosts();
   }, [page]);
 
   return (
       <div className="App">
+
         <CustomButton style={{marginLeft: 240, marginTop: 10}} onClick={() => setModal(true)}>
           Создать пост
         </CustomButton>
+
         <Modal visible={modal} setVisible={setModal}>
           <PostForm create={createPost}/>
         </Modal>
+
         <hr style={{margin: '15px 0'}}/>
+
         <PostFilter
-            filter={filter}
-            setFilter={setFilter}
+          filter={filter}
+          setFilter={setFilter}
         />
+
+        <PostList remove={removePost} posts={searchedAndSortedPosts} title={'Список постов'}/>
+        <div ref={lastElement}/>
         {
-          isPostsLoading
-              ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}> <Loader/> </div>
-              : <PostList remove={removePost} posts={searchedAndSortedPosts} title={'Список постов 1'}/>
+          isPostsLoading  &&
+             <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}> <Loader/> </div>
         }
+
         {
             postError
             && <h1>Ошибка: ${postError}</h1>
         }
+        
         <Pagination pages={pagesArray} page={page} setPage={setPage}/>
+
       </div>
   );
 }
